fdc8ce089009f6672b91ceb712a38c8d2e4369a8
Daniyar Slamkul <d.slamkul@aviata.me>
2023-07-30 13:31:31 +0600

    material components part

--- app/src/main/java/dev/dslam/vkapiclientbydanik/MainActivity.kt
+++ app/src/main/java/dev/dslam/vkapiclientbydanik/MainActivity.kt
@@ -1,24 +1,93 @@
 package dev.dslam.vkapiclientbydanik
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.dslam.vkapiclientbydanik.ui.theme.VkApiClientByDanikTheme
+import dev.dslam.vkapiclientbydanik.ui.theme.VkPostCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VkApiClientByDanikTheme {
-
-            }
+            AppBarTest()
         }
+    }
+}
+
+@Composable
+fun AppBarTest() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "TopAppBarTitle") },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Menu, contentDescription = null)
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            val items = listOf("Songs", "Artists", "Playlists")
+            BottomAppBar {
+                BottomNavigation() {
+                    BottomNavigationItem(
+                        selected = true,
+                        onClick = { /*TODO*/ },
+                        icon = {
+                            Icon(Icons.Filled.Favorite, contentDescription = null)
+                        },
+                        label = {
+                            Text(text = "Favorite")
+                        }
+                    )
+
+                    BottomNavigationItem(
+                        selected = true,
+                        onClick = { /*TODO*/ },
+                        icon = {
+                            Icon(Icons.Outlined.Edit, contentDescription = null)
+                        },
+                        label = {
+                            Text(text = "Edit")
+                        }
+                    )
+
+                    BottomNavigationItem(
+                        selected = true,
+                        onClick = { /*TODO*/ },
+                        icon = {
+                            Icon(Icons.Outlined.Delete, contentDescription = null)
+                        },
+                        label = {
+                            Text(text = "Delete")
+                        }
+                    )
+                }
+            }
+        },
+        drawerContent = {
+            Text(text = "text 1")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "text 2")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "text 3")
+        }
+    ) {
+
     }
 }
--- app/src/main/java/dev/dslam/vkapiclientbydanik/ui/theme/VkPostCard.kt
+++ app/src/main/java/dev/dslam/vkapiclientbydanik/ui/theme/VkPostCard.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,12 +18,12 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.dslam.vkapiclientbydanik.R
 
-@Preview
 @Composable
 fun VkPostCard() {
 
@@ -32,38 +36,116 @@ fun VkPostCard() {
             modifier = Modifier
                 .padding(4.dp)
         ) {
-            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-                Image(
-                    modifier = Modifier
-                        .size(30.dp)
-                        .clip(CircleShape)
-                        .background(Color.Black),
-                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                    contentDescription = ""
-                )
+            VkPostCardHeader()
+            VkPostCardBody()
+            VkPostCardFooter()
+        }
+    }
+}
 
-                Column(modifier = Modifier.absolutePadding(4.dp)) {
-                    Text(text = "Уволено", fontWeight = FontWeight.Bold, color = Color.Black)
-                    Text(text = "14:00", color = Color.Gray)
-                }
+@Composable
+fun VkPostCardHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp), verticalAlignment = Alignment.CenterVertically
+    ) {
 
-                Spacer(modifier = Modifier.weight(1f))
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .background(Color.Black),
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = null
+        )
 
-                Image(painter = painterResource(id = R.drawable.ic_baseline_more_vert_24), contentDescription = "")
-            }
+        Spacer(modifier = Modifier.width(8.dp))
 
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Уволено", fontWeight = FontWeight.Bold, color = MaterialTheme.colors.onPrimary)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = "14:00", color = MaterialTheme.colors.onSecondary)
+        }
+
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = null,
+            tint = MaterialTheme.colors.onSecondary
+        )
+    }
+}
+
+@Composable
+fun VkPostCardBody() {
+    Text(
+        text = stringResource(R.string.template_text)
+    )
+
+    Image(
+        modifier = Modifier.fillMaxWidth(),
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "",
+        contentScale = ContentScale.FillWidth
+    )
+}
+
+@Composable
+fun VkPostCardFooter() {
+    Row(
+        modifier = Modifier.padding(top = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(painter = painterResource(id = R.drawable.ic_views_count), contentDescription = null)
             Text(
-                text = "кабаныч когда узнал, что если сотрудникам не платить они начинают умирать от голода",
-                color = Color.Black, modifier = Modifier.absolutePadding(top = 8.dp, bottom = 8.dp)
+                modifier = Modifier.absolutePadding(left = 4.dp),
+                text = "916",
+                color = MaterialTheme.colors.onSecondary
             )
+        }
 
-            Image(
-                modifier = Modifier.fillMaxWidth(),
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = "",
-                contentScale = ContentScale.FillWidth
+        Spacer(modifier = Modifier.weight(2f))
+
+        Row(modifier = Modifier.weight(1f), verticalAlignment = Alignment.CenterVertically) {
+            Image(painter = painterResource(id = R.drawable.ic_share), contentDescription = null)
+            Text(
+                modifier = Modifier.absolutePadding(left = 4.dp),
+                text = "7",
+                color = MaterialTheme.colors.onSecondary
+            )
+        }
+        Row(modifier = Modifier.weight(1f), verticalAlignment = Alignment.CenterVertically) {
+            Image(painter = painterResource(id = R.drawable.ic_comment), contentDescription = null)
+            Text(
+                modifier = Modifier.absolutePadding(left = 4.dp),
+                text = "8",
+                color = MaterialTheme.colors.onSecondary
+            )
+        }
+        Row(modifier = Modifier.weight(1f), verticalAlignment = Alignment.CenterVertically) {
+            Image(painter = painterResource(id = R.drawable.ic_like), contentDescription = null)
+            Text(
+                modifier = Modifier.absolutePadding(left = 4.dp),
+                text = "23",
+                color = MaterialTheme.colors.onSecondary
             )
         }
     }
+}
 
+@Preview
+@Composable
+fun PreviewLight() {
+    VkApiClientByDanikTheme(darkTheme = false) {
+        VkPostCard()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewDark() {
+    VkApiClientByDanikTheme(darkTheme = true) {
+        VkPostCard()
+    }
 }
