bc74177179b7faaed3e7c178e9e94169f09c56fb
Daniyar Slamkul <d.slamkul@aviata.me>
2023-07-31 12:55:55 +0600

    floating action button

--- app/src/main/java/dev/dslam/vkapiclientbydanik/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/dev/dslam/vkapiclientbydanik/ui/theme/VkNewsMainScreen.kt
@@ -1,14 +1,27 @@
 package dev.dslam.vkapiclientbydanik.ui.theme
 
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen() {
+    val snackbarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
         bottomBar = {
             BottomNavigation {
                 val items = listOf(NavigationItem.Home, NavigationItem.Favorite, NavigationItem.Profile)
@@ -30,6 +43,25 @@ fun MainScreen() {
                         selectedContentColor = MaterialTheme.colors.onPrimary,
                         unselectedContentColor = MaterialTheme.colors.onSecondary
                     )
+                }
+            }
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(onClick = {
+                    scope.launch {
+                        val action = snackbarHostState.showSnackbar(
+                            "This is SnackBar",
+                            actionLabel = "Hide FAB",
+                            duration = SnackbarDuration.Long
+                        )
+
+                        if (action == SnackbarResult.ActionPerformed) {
+                            fabIsVisible.value = false
+                        }
+                    }
+                }) {
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
                 }
             }
         }
